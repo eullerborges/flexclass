7d7394e03fa59cefb73bfad80c8cc136136f8d01
github-action-benchmark <github@users.noreply.github.com>
2020-10-03 16:46:51 +0000

    add Catch2 Benchmark (catch2) benchmark result for 4bdc347b2fd38f7eaeb2af7a64ce7b355bf76e38

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601743579995,
+  "lastUpdate": 1601743611389,
   "repoUrl": "https://github.com/eullerborges/flexclass",
   "entries": {
     "Catch2 Benchmark": [
@@ -78,6 +78,86 @@ window.BENCHMARK_DATA = {
             "name": "Traverse DAG with fc",
             "value": 2.79104,
             "range": "± 53.7827",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eullerborges@opacium.com",
+            "name": "Euller Borges",
+            "username": "eullerborges"
+          },
+          "committer": {
+            "email": "eullerborges@opacium.com",
+            "name": "Euller Borges",
+            "username": "eullerborges"
+          },
+          "distinct": true,
+          "id": "4bdc347b2fd38f7eaeb2af7a64ce7b355bf76e38",
+          "message": "Attempt benchmark regression",
+          "timestamp": "2020-10-03T13:45:11-03:00",
+          "tree_id": "2c01c168a20cf017b5492d1fa96030ec8575b348",
+          "url": "https://github.com/eullerborges/flexclass/commit/4bdc347b2fd38f7eaeb2af7a64ce7b355bf76e38"
+        },
+        "date": 1601743610748,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Sum all ids on nofc graph",
+            "value": 26.449,
+            "range": "± 5.96597",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          },
+          {
+            "name": "Sum all ids on withfc graph",
+            "value": 25.227,
+            "range": "± 4.41943",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          },
+          {
+            "name": "Sum all link ptrs on nofc graph",
+            "value": 28.6101,
+            "range": "± 3.85415",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          },
+          {
+            "name": "Sum all link ptrs on withfc graph",
+            "value": 5.48199,
+            "range": "± 7.77457",
+            "unit": "us",
+            "extra": "100 samples\n9 iterations"
+          },
+          {
+            "name": "Create DAG no fc",
+            "value": 2.55519,
+            "range": "± 327.037",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "Create DAG with fc",
+            "value": 1.82731,
+            "range": "± 230.049",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "Traverse DAG no fc",
+            "value": 3.44173,
+            "range": "± 362.527",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "Traverse DAG with fc",
+            "value": 3.36666,
+            "range": "± 439.191",
             "unit": "ms",
             "extra": "100 samples\n1 iterations"
           }
